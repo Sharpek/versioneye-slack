eacab360237224e3623de25053f7514b4f7aae8c
Marcin Baran <sharpek@sharpek.net>
2017-01-12 13:57:35 +0100

    Change setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,8 @@
-from codecs import open
 from os import path
 
 from setuptools import setup
 
 here = path.abspath(path.dirname(__file__))
-
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
 
 setup(
     name='versioneye_slack',
@@ -14,7 +10,7 @@ setup(
     version='1.0',
 
     description='Send notifications from versioneye to slack channel Edit',
-    long_description=long_description,
+    long_description='Send notifications from versioneye to slack channel Edit',
     url='https://github.com/Sharpek/versioneye-slack',
     author='Marcin Baran',
     author_email='sharpek@sharpek.net',
